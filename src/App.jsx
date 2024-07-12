b3bb46b4bc6aaa3835803754f64e82929c2923bb
Manas0066 <manaschaudhary1602@gmail.com>
2024-07-13 04:32:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Navbar from './components/navbar';
+import Hero from './components/Hero';
+import About from './components/About'
+import Technologies from './components/Technologies';
+import Experiance from './components/Experiance';
+import Project from './components/Project';
+import Contact from './components/Contact';
+import LocomotiveScroll from 'locomotive-scroll';
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  const locomotiveScroll = new LocomotiveScroll();
+  return (
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className='fixed top-0 left-0 z-[-10] h-full w-full'>
+      <div className="absolute top-0 left-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse at 50% -20%, rgba(120, 119, 198, 0.3), rgba(255, 255, 255, 0))]"></div>
+      </div>
+      <div className='container mx-auto px-8'>
+        <Navbar />
+        <Hero />
+        <About/>
+        <Technologies/>
+        <Experiance/>
+        <Project/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import logo from '../assets/kevinRushLogo.png';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+const navbar = () => {
+  return (
+    <>
+    <nav className=' mb-20 flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 item-center'>
+        <img  className='mx-2 w-10' src={logo} alt="logo"/>
+      </div>
+      <div className='flex m-8 item-center justify-center gap-4 text-2xl'>
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaTwitter/>
+        <FaInstagram/>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default navbar
